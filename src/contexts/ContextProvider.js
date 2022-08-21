699645ffa70c826489824e078dc4fad42308c263
unknown <supertrampsrp@gmail.com>
2022-08-21 19:16:05 +0530

    first commit

---
+++ src/contexts/ContextProvider.js
@@ -0,0 +1,53 @@
+import React, {createContext, useContext, useState} from "react";
+
+const StateContext = createContext();
+
+const initialState = {
+    chat : false,
+    chart : false,
+    userProfile : false,
+    notification : false
+}
+export const ContextProvider =({children}) => {
+    const [activeMenu, setActiveMenu] =useState (true);
+    const [isClicked, setIsClicked] =useState (initialState);
+    const [screenSize, setScreenSize] = useState (undefined);
+    const [currentColor, setcurrentColor] = useState('#03C9D7');
+    const [currentMode, setcurrentMode] = useState('Light');
+    const [themeSettings, setthemeSettings] = useState(false)
+    const setMode = (e) => {
+        setcurrentMode(e.target.value);
+        localStorage.setItem('themeMode', e.target.value);
+        setthemeSettings(false);
+    }
+    const setColor = (color) => {
+        setcurrentColor(color);
+        localStorage.setItem('colorMode', color);
+        setthemeSettings(false);
+    }
+    const handleClick = (clicked)=>{
+        setIsClicked({...initialState, clicked:true});
+    }
+    return (
+        <StateContext.Provider
+        value={{
+            activeMenu,
+            setActiveMenu,
+            isClicked, 
+            setIsClicked,
+            handleClick,
+            screenSize,
+            setScreenSize,
+            currentColor,
+            currentMode,
+            setMode, setColor,
+            themeSettings,
+            setthemeSettings
+            
+        }}
+        >
+            {children}
+        </StateContext.Provider>
+    )
+}
+export const useStateContext = () => useContext (StateContext);
